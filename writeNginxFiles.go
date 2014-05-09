370e79ee849bafa677e6fc254846eb6bdb847707
Benton Roberts <broberts@mdsol.com>
2014-05-09 12:29:20 -0700

    remove conf files for nonexistent apps

--- writeNginxFiles.go
+++ writeNginxFiles.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/coreos/go-etcd/etcd"
+	"io/ioutil"
 	"os"
+	"regexp"
 )
 
+// Makes this app's conf files less likely to collide with others'
+const NGINX_SUFFIX = "-etcd-vhost.conf"
+
+// Returns the name of an Nginx configuration file for the given web app
+func confFileName(app *Webapp) string {
+	return fmt.Sprintf("%s%s", app.ID, NGINX_SUFFIX)
+}
+
+// Queries etcd, then writes out an NGinx configuration file for each app.
+// Finally, calls removeOldNginxFiles to remove outdated conf files.
 func writeNginxFiles(client *etcd.Client, config *Config) error {
 	apps, err := readAppsFromEtcd(client, config.Prefix)
 	if err == nil {
 		for _, app := range apps {
-			outPath := fmt.Sprintf("%s/%s.conf", config.Outdir, app.ID)
+			outPath := fmt.Sprintf("%s/%s", config.Outdir, confFileName(&app))
 			fmt.Printf("Writing file: %s...\n", outPath)
 			var outFile *os.File
 			outFile, err = os.Create(outPath)
@@ -23,6 +35,32 @@ func writeNginxFiles(client *etcd.Client, config *Config) error {
 				err = app.Render(outFile)
 				if err != nil {
 					return err
+				}
+			}
+		}
+		err = removeOldNginxFiles(config, apps)
+	}
+	return err
+}
+
+// Deletes configuration files for apps that are no longer in etcd
+func removeOldNginxFiles(config *Config, apps []Webapp) error {
+	webappFile, _ := regexp.Compile(fmt.Sprintf("%s$", NGINX_SUFFIX))
+	validFiles := map[string]bool{} // create a map of valid filenames
+	for _, app := range apps {
+		validFiles[confFileName(&app)] = true
+	}
+	files, err := ioutil.ReadDir(config.Outdir) // list all existing conf files
+	if err == nil {
+		for _, f := range files {
+			if webappFile.Match([]byte(f.Name())) {
+				if _, valid := validFiles[f.Name()]; !valid {
+					outpath := fmt.Sprintf("%s/%s", config.Outdir, f.Name())
+					fmt.Println("WARNING: deleting file", outpath)
+					err = os.Remove(outpath)
+					if err != nil {
+						return err
+					}
 				}
 			}
 		}
